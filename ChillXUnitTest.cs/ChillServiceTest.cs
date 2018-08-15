0b7fd00dbdc1907c6ad5d6ce24bf341d8bc75653
Shaurav Adhikari <shauravraj@hotmail.com>
2018-08-14 20:03:41 -0500

    adding more test cases

--- ChillXUnitTest.cs/ChillServiceTest.cs
+++ ChillXUnitTest.cs/ChillServiceTest.cs
@@ -6,6 +6,7 @@ using Moq;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace ChillXUnitTest.cs
@@ -40,8 +41,26 @@ namespace ChillXUnitTest.cs
             Assert.Equal(accountInfoResult.Balance, account.Balance);
             Assert.Equal(accountInfoResult.Currency, account.Currency);
             Assert.Equal(accountInfoResult.AccountNumber, account.AccountNumber);
+            Assert.True(accountInfoResult.Successful);
             repository.Verify(s => s.Get(accountNumber), Times.Once);
         }
 
+        [Fact]
+        public void Get_TakesAccountNumber_Returns_AccountInfo_Fails()
+        {
+            //Arrange
+            var accountNumber = 1;
+            Account account = null;
+            repository.Setup(s => s.Get(accountNumber)).Returns(account).Verifiable();
+            //Act
+            var accountInfoResult = service.Get(accountNumber);
+            //Assert
+            Assert.NotNull(accountInfoResult);
+            Assert.IsType<AccountResultVM>(accountInfoResult);
+            Assert.Equal(0, accountInfoResult.Balance);
+            Assert.Null(accountInfoResult.Currency);
+            Assert.False(accountInfoResult.Successful);
+            repository.Verify(s => s.Get(accountNumber), Times.Once);
+        }
     }
 }
